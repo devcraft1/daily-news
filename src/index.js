9a6787144235e771a3f3427998db852f29c4d7e4
devcraft1 <ukonudennis64@gmail.com>
2021-11-15 18:20:51 +0100

    fix/render

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './pages/app';
-import './style/global.scss'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- src/pages/app.js
+++ src/pages/app.js
@@ -1,16 +1,11 @@
 import React from "react";
-import MainPage from "./main";
 import { Switch, Route, Router } from "../util/router.js";
-import AboutPage from "../component/AboutPage";
 import API from "../api/API";
 
 function App() {
   return (
     <Router>
       <Switch>
-        {/* <Route exact path="/news" component={API} />
-        <Route exact path="/gallary" component={MainPage} />
-        <Route exact path="/about" component={AboutPage} /> */}
         <Route exact path="/" component={API} />
         <Route
           component={({ location }) => {
