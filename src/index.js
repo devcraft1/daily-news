706ae27bf765b8605f751289161f60f17240240d
devcraft1 <ukonudennis64@gmail.com>
2021-11-16 09:15:45 +0100

    fix/index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './pages/app';
-// import * as serviceWorker from './serviceWorker';
 import API from './api/API'
 
 ReactDOM.render(<API />, document.getElementById('root'));
